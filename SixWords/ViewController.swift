ec803668d051a075a5a498c2e4b63a33402f2985
Matt Hanlon <mhanlon@thecodehub.ie>
2021-03-02 21:45:24 +0000

    Added one IBOutlet and some simple animations of our surfer image view.

--- SixWords/ViewController.swift
+++ SixWords/ViewController.swift
@@ -9,12 +9,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet var surferView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        animate()
     }
 
+    func animate() {
+        Timer.scheduledTimer(withTimeInterval: 3.0, repeats: false) { (timer) in
+            self.surferView.image = self.surferView.image?.withHorizontallyFlippedOrientation()
+            UIView.animate(withDuration: 5.0) {
+                self.surferView.frame = CGRect(x: self.view.frame.size.width, y: (self.view.frame.size.height/2), width: self.surferView.frame.size.width, height: self.surferView.frame.size.height)
+            }
+        }
+    }
 
 }
 
